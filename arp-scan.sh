b331b08001b3e9fa730b65486893a940e61a813b
Kazushi Takamura <sakamatapd5@gmail.com>
2019-02-28 05:41:29 +0900

    Add: 一部のみを表記した MACAddressを送信情報に追加 最初と最後の2文字づつのみを送信

--- arp-scan.sh
+++ arp-scan.sh
@@ -38,7 +38,10 @@ do
     # mac+=(`echo $line | grep -io '[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}'`)
     pure_mac=(`echo $line | grep -io '[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}'`)
     hash_mac+=(`echo -n ${pure_mac^^}$secret | shasum -a 256 | awk '{print $1}'`)
-
+    # 省略型MACAddressの生成 ex, 12:EF
+    upper_mac=${pure_mac^^}
+    str=":"
+    omit_mac+=(`echo ${upper_mac:0:2}$str${upper_mac:15:2}`)
     # 配列変数ipに値を追加、ipの配列を作る
     ip+=(`echo $line | grep -Eio '^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}'`)
     # 3番目の要素 vendorを配列変数に追加
@@ -60,7 +63,7 @@ time=$(date +%s)
 hash=(`echo -n $time$secret | shasum -a 256 | awk '{print $1}'`)
 
 # テキストをjson化する。
-json=$(jo status=$trigger hash=$hash time=$time community_id=$community_id router_id=$router_id mac=$(jo "${hash_mac[@]}" -a)  vendor=$(jo "${vendor[@]}" -a))
+json=$(jo status=$trigger hash=$hash time=$time community_id=$community_id router_id=$router_id mac=$(jo "${hash_mac[@]}" -a) omit_mac=$(jo "${omit_mac[@]}" -a) vendor=$(jo "${vendor[@]}" -a))
 # echo -e $json
 
 ##### 環境毎にurl 変更を行うこと ######
@@ -79,7 +82,7 @@ then
   name=$(echo $res | jq ".name" | sed "s/\"//g")
   message=$(echo $res | jq ".message" | sed "s/\"//g")
   # 自宅テスト用
-#   node /home/pi/GoogleHomeTalk.js $IP $name $message
+  # node /home/pi/GoogleHomeTalk.js $IP $name $message
   # 本番環境 
   node /home/pi/local_mac_address_collect_post/GoogleHomeTalk.js $IP $name $message
 fi
